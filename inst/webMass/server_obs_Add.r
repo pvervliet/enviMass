4fd0ed3f755ddf093a320c98f2606374425abd78
blosloos <blosloos@users.noreply.github.com>
2018-01-22 09:17:05 +0100

    debug delete file

--- inst/webMass/server_obs_Add.r
+++ inst/webMass/server_obs_Add.r
@@ -1012,7 +1012,7 @@ observe({
 			names_neg<-measurements3[
 				(measurements3[,"Mode"]=="negative") & (measurements3[,"Type"]!="sample")
 			,"Name"]
-			IDs_neg<-paste(IDs_neg,names_pos,sep=" - ")
+			IDs_neg<-paste(IDs_neg, names_neg, sep = " - ")
 			if(any(logfile[[14]]!="FALSE")){
 				select_neg<-logfile[[14]]
 				select_neg<-select_neg[select_neg!="FALSE"]
